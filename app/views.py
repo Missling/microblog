93a2ea6d41e8b8780aaf6a6ece028b9aff13c320
missling <ling.giang@gmail.com>
2015-11-26 18:42:21 -0800

    add openid to login

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -20,4 +21,21 @@ def index():
                            title='Home',
                            user=user,
                            posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+
+def login():
+  # imported LoginForm class, instantiated an object from it, and sent it down to the template
+  form = LoginForm() 
+
+  # validate_on_submit is called as part of a form submission request, it will gather all the data, run all the validators attached to fields, and if everything is all right it will return True, indicating that the data is valid and can be processed.
+  if form.validate_on_submit():
+    flash('Login requested for OpenID="%s", remember_me=%s' %
+      (form.openid.data, str(form.remember_me.data)))
+    return redirect('/index')
+  return render_template('login.html', 
+                           title='Sign In',
+                           form=form,
+                           providers=app.config['OPENID_PROVIDERS'])
+
     
